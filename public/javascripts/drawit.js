26008a1a5a0cf35bc6462591fff834450f203a10
Jon T. Anderson <jontanderson@gmail.com>
2014-03-28 06:45:34 -0400

    Added rooms and users to the Socket.io connection

--- public/javascripts/drawit.js
+++ public/javascripts/drawit.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	var socket = io.connect();
 	var $message = $("#message");
 	var $chatwindow = $("#chatwindow");
+	var $userlist = $("#userlist");
 	
 	$("#sendmessageform").submit(function(e) {
 		e.preventDefault();
@@ -12,8 +13,35 @@ $(document).ready(function() {
 		socket.emit('sendmessage',data);
 	});
 	
+	$("#userform").submit(function(e) {
+		e.preventDefault();
+		var data = {
+									username : $("#username").val(),
+									room     : $("#room").val()
+							 };
+		socket.emit('newuser', data,function(valid) {
+			if (valid) {
+				$("#loginwrapper").hide();
+				$("#mainwrapper").show();
+			} else {
+				alert("That username is already in use.  Please choose another.");				
+			}
+			$("#username").val('');
+			$("#room").val('');
+		});
+	});
+	
+	socket.on('usernames',function(usernames) {
+		$("#userlist").empty();
+		for (i=0;i<usernames.length;i++) {
+			$("#userlist").append("<li class='user'>" + usernames[i] + "</li>");			
+		}
+	});
+	
+	socket.on('userjoined',function(username) {
+		$chatwindow.prepend("<li class='message'><span class='user'>" + username + "</span> joined the game.");
+	});
 	socket.on('newmessage',function(data) {
-		alert(data['message']);
 		$chatwindow.append("<li class='message'>" + data['message'] + "</li>");
 	});
 });
--- routes/drawit.js
+++ routes/drawit.js
@@ -1,14 +1,41 @@
+
 exports.configure = function(server)
 {
 	// setup Socket.IO
 	var io = require('socket.io').listen(server);
-	
+	var users = [];
 	// listen for connection
 	io.sockets.on('connection', function(socket) {
 		// listen for sent messages and process them
 		socket.on('sendmessage',function(data) {
-			io.sockets.emit('newmessage',data);
-		});		
+			io.sockets.in(socket.room).emit('newmessage',data);
+		});
+		
+		socket.on('newuser',function(data,callback) {
+			if (users.indexOf(data["username"]) != -1)
+			{
+				callback(false);
+			} else {
+				callback(true);
+				socket.username = data['username'];
+				socket.room = data['room'];
+				socket.join(data['room']);
+				if (!users[data['room']]) {
+					users[data['room']] = new Array();
+				}
+				users[data['room']].push(data['username']);
+				io.sockets.in(data['room']).emit('usernames',users[data['room']]);
+				io.sockets.in(data['room']).emit('userjoined',data['username']);
+			}
+		});
+		
+		socket.on('disconnect',function() {
+			console.log(socket.username + " disconnected from room '" + socket.room + "'");
+			if (!socket.username) return;
+			users[socket.room].splice(users.indexOf(socket.username),1);
+			io.sockets.in(socket.room).emit('usernames',users[socket.room]);
+			io.sockets.in(socket.room).emit('userleft',socket.username);
+		});
 	});
 };
 
